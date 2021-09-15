47ef9b593d4778d8e5bf5a46dda04f3af42aa5c7
Brian Foster <brianisbrilliant@gmail.com>
2021-09-15 07:22:53 -0500

    updated galleryScene

--- Assets/ButtonController.cs
+++ Assets/ButtonController.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
+    public bool buttonIsEnemy = true;
+    private UIController ui;
 
-    // todo, find scorekeeper and change score.
+    // find scorekeeper to change score.
+    void Start() {
+        ui = GameObject.Find("Canvas").GetComponent<UIController>();
+
+        if(!buttonIsEnemy) {
+            Destroy(this.gameObject, 3);
+        }
+    }
 
     public void Pressed() {
+        if(buttonIsEnemy) {
+            ui.UpdateScore(1);
+        } else {
+            ui.UpdateScore(-5);
+        }
+        
         Destroy(this.gameObject);
     }
 }
--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -7,12 +7,13 @@ using TMPro;
 public class UIController : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public string scoreName = "Coins";
 
     private int totalCoins = 0;
 
     public void UpdateScore(int givenChange = 1) {
         totalCoins += givenChange;
-        scoreText.text = "Coins: " + totalCoins;
+        scoreText.text = scoreName + ": " + totalCoins;
     }
 
 
